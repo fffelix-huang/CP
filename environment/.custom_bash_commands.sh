f589d8bce1da84e1407f96266c9b7b35906ab29d
fffelix-huang <72808219+fffelix-huang@users.noreply.github.com>
2021-12-16 21:51:11 +0800

    Create .custom_bash_commands.sh

---
+++ environment/.custom_bash_commands.sh
@@ -0,0 +1,25 @@
+function make() {
+	echo -e '\033[1;32m[No flag]\033[m Compiling' $1'.cpp with c++17.'
+	'g++' '-std=c++17' '-I' '.' $1'.cpp' '-o' $1
+}
+
+function fast() {
+	echo -e '\033[1;35m[-O2 flag]\033[m Compiling' $1'.cpp with c++17.'
+	'g++' '-std=c++17' '-I' '.' '-O2' $1'.cpp' '-o' $1
+}
+
+function debug() {
+	echo -e '\033[1;31m[DEBUG MODE]\033[m Compiling' $1'.cpp with c++17.'
+	'g++' '-std=c++17' '-I' '.' '-DLOCAL' $1'.cpp' '-o' $1
+}
+
+function submit() {
+	echo 'Expanding' $1'.cpp'
+	'python3' 'expander.py' $1'.cpp'
+	clip.exe < combined.cpp
+}
+
+function open() {
+	echo 'Opening' $1
+	'cmd.exe' '/C' 'start' $1
+}
