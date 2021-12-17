423c881c4c943b62f02365d75dcbb2cea3b21438
fffelix-huang <72808219+fffelix-huang@users.noreply.github.com>
2021-12-17 22:33:17 +0800

    Update .custom_bash_commands.sh

--- environment/.custom_bash_commands.sh
+++ environment/.custom_bash_commands.sh
@@ -13,6 +13,32 @@ function debug() {
 	'g++' '-std=c++17' '-I' '.' '-DLOCAL' $1'.cpp' '-o' $1
 }
 
+function stress_test() {
+	# $1 : gen
+	# $2 : brute
+	# $3 : sol
+	fast $1
+	fast $2
+	fast $3
+	for((i = 1; ; ++i)); do
+		./$1 $i > debug_in
+		./$3 < debug_in > my_out
+		./$2 < debug_in > ans_out
+		diff -Z my_out ans_out || break
+		echo -e "\033[1;32mPassed test: " $i "\033[m"
+	done
+
+	echo -e "\033[1;45mWA on the following test\033[m"
+	echo -e "\033[0;46mInput\033[m"
+	cat debug_in
+	echo ""
+	echo -e "\033[1;41mYour answer\033[m"
+	cat my_out
+	echo ""
+	echo -e "\033[1;42mCorrect answer\033[m"
+	cat ans_out
+}
+
 function submit() {
 	echo 'Expanding' $1'.cpp'
 	'python3' 'expander.py' $1'.cpp'
