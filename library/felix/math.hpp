a7f08df6c1de233057e4981cac50051a8f9f0e7e
fffelix-huang <72808219+fffelix-huang@users.noreply.github.com>
2022-01-10 14:44:43 +0800

    Add files via upload

---
+++ library/felix/math.hpp
@@ -0,0 +1,41 @@
+#ifndef FELIX_MATH_HPP
+#define FELIX_MATH_HPP 1
+
+#include <cassert>
+#include <felix/internal_math>
+
+namespace felix {
+
+template<class T>
+T power(T a, unsigned long long b) {
+	assert(b >= 0);
+	T ans = 1;
+	while(b) {
+		if(b & 1)
+			ans *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return ans;
+}
+
+long long floor_sum(long long n, long long m, long long a, long long b) {
+    assert(0 <= n && n < (1LL << 32));
+    assert(1 <= m && m < (1LL << 32));
+    unsigned long long ans = 0;
+    if (a < 0) {
+        unsigned long long a2 = internal::safe_mod(a, m);
+        ans -= 1ULL * n * (n - 1) / 2 * ((a2 - a) / m);
+        a = a2;
+    }
+    if (b < 0) {
+        unsigned long long b2 = internal::safe_mod(b, m);
+        ans -= 1ULL * n * ((b2 - b) / m);
+        b = b2;
+    }
+    return ans + internal::floor_sum_unsigned(n, m, a, b);
+}
+
+} // namespace felix
+
+#endif // FELIX_MATH_HPP
---
+++ library/felix/tree.hpp
@@ -0,0 +1,77 @@
+#ifndef FELIX_TREE_HPP
+#define FELIX_TREE_HPP 1
+
+#include <cassert>
+#include <vector>
+#include <functional>
+#include <felix/graph>
+
+namespace felix {
+
+template<class T>
+class tree : public graph<T> {
+public:
+	using graph<T>::n;
+	using graph<T>::edges;
+	using graph<T>::g;
+
+	std::vector<int> depth, parent, sz;
+
+	tree() : tree(0) {}
+
+	tree(int _n) : graph<T>(_n), depth(std::vector<int>(_n)), parent(std::vector<int>(_n)), sz(std::vector<int>(_n)) {}
+
+	virtual void add_edge(int from, int to, T cost = 1) {
+		assert(0 <= from && from < n);
+		assert(0 <= to && to < n);
+		int id = int(edges.size());
+		assert(id < n - 1);
+		g[from].push_back(id);
+		g[to].push_back(id);
+		edges.push_back({from, to, id});
+	}
+
+	void clear() {
+		depth.clear();
+		parent.clear();
+		sz.clear();
+	}
+
+	void build_tree(int root) {
+		assert(0 <= root && root < n);
+		std::function<void(int, int)> dfs = [&](int u, int p) {
+			depth[u] = (u == root ? 0 : depth[p] + 1);
+			parent[u] = p;
+			sz[u] = 1;
+			for(const int& id : g[u]) {
+				auto& e = edges[id];
+				int v = e.from ^ e.to ^ u;
+				if(v == p)
+					continue;
+				dfs(v, u);
+				sz[u] += sz[v];
+			}
+		};
+		dfs(root, -1);
+	}
+
+	int find_centroid() {
+		build_tree(0);
+		std::function<int(int, int)> dfs = [&](int u, int p) -> int {
+			for(const int& id : g[u]) {
+				auto& e = edges[id];
+				int v = e.from ^ e.to ^ u;
+				if(v == p)
+					continue;
+				if(sz[v] > (n >> 1))
+					return dfs(v, u);
+			}
+			return u;
+		};
+		return dfs(0, -1);
+	}
+};
+
+} // namespace felix
+
+#endif // FELIX_TREE_HPP
