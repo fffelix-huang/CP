bb09229ad9569f1a1b3a55fe80fc109e29bb88ab
fffelix-huang <72808219+fffelix-huang@users.noreply.github.com>
2022-01-19 20:56:42 +0800

    Update .custom_bash_commands.sh

--- environment/.custom_bash_commands.sh
+++ environment/.custom_bash_commands.sh
@@ -1,26 +1,27 @@
 function make() {
 	echo -e '\033[1;42m[No flag]\033[m Compiling' $1'.cpp with c++17.'
-	'g++' '-std=c++17' '-I' '.' $1'.cpp' '-o' $1
+	g++ -std=c++17 -I ~/../../mnt/c/Felix/cp/library $1.cpp -o $1
 }
 
 function fast() {
 	echo -e '\033[1;45m[-O2 flag]\033[m Compiling' $1'.cpp with c++17.'
-	'g++' '-std=c++17' '-I' '.' '-O2' $1'.cpp' '-o' $1
+	g++ -std=c++17 -I ~/../../mnt/c/Felix/cp/library -O2 $1.cpp -o $1
 }
 
 function debug() {
 	echo -e '\033[1;41m[DEBUG MODE]\033[m Compiling' $1'.cpp with c++17.'
-	'g++' '-std=c++17' '-I' '.' '-DLOCAL' $1'.cpp' '-o' $1
+	g++ -std=c++17 -I ~/../../mnt/c/Felix/cp/library -DLOCAL $1.cpp -o $1
 }
 
 function run_stress_test() {
 	# $1 : gen
 	# $2 : brute
 	# $3 : sol
+	# $1 : starting test case id
 	fast $1
 	fast $2
 	fast $3
-	for((i = 1; ; ++i)); do
+	for((i = $4; ; ++i)); do
 		./$1 $i > debug_in
 		./$3 < debug_in > my_out
 		./$2 < debug_in > ans_out
@@ -41,11 +42,11 @@ function run_stress_test() {
 
 function submit() {
 	echo 'Expanding' $1'.cpp'
-	'python3' 'expander.py' $1'.cpp'
+	python3 ~/../../mnt/c/Felix/cp/library/expander.py $1.cpp --lib ~/../../mnt/c/Felix/cp/library
 	clip.exe < combined.cpp
 }
 
 function open() {
 	echo 'Opening' $1
-	'cmd.exe' '/C' 'start' $1
+	cmd.exe /C start $1
 }
